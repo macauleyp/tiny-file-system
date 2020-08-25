d99c4ee673d59f35c868604170367224081d022d
55mac24 <macauleypinto@gmail.com>
2020-08-25 17:04:34 -0400

    Update block.h

--- block.h
+++ block.h
@@ -1,14 +1,3 @@
-/*
- *  Copyright (C) 2019 CS416 Spring 2019
- *	
- *	Tiny File System
- *
- *	File:	block.h
- *  Author: Yujie REN
- *	Date:	April 2019
- *
- */
-
 #ifndef _BLOCK_H_
 #define _BLOCK_H_
 
